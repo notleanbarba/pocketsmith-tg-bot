41cef05d226b3f72f02313d5895d667a7deb8d8b
notleanbarba <leanbarba@protonmail.com>
2024-02-10 12:45:20 -0700

    add config.py script

---
+++ scripts/config.py
@@ -0,0 +1,18 @@
+import os
+import toml
+
+
+if __name__ == "__main__":
+
+    with open("config.toml", "r") as c:
+        user_config = toml.load(c)
+
+    print("Saving enviroment variables to config.toml")
+
+    user_config["Pocketsmith"]["pocketsmith_token"] = os.getenv("POCKETSMITH_TOKEN")
+    user_config["Telegram"]["telegram_bot_token"] = os.getenv("TELEGRAM_TOKEN")
+    user_config["Bot"]["private_ip"] = os.getenv("PRIVIP")
+    user_config["Bot"]["domain"] = os.getenv("DOMAIN")
+
+    with open("config.toml", "wb") as c:
+        toml.dump(user_config, c)
