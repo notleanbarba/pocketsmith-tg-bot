7ba52e0cd95df5b2bcb064cfe3cea8af260b4358
notleanbarba <leanbarba@protonmail.com>
2024-02-13 21:38:17 -0700

    Change working directory

--- py/config.py
+++ py/config.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
 
     user_config["Pocketsmith"]["pocketsmith_token"] = os.getenv("POCKETSMITH_TOKEN")
     user_config["Pocketsmith"]["user"] = os.getenv("PS_USER")
+    user_config["Pocketsmith"]["user"] = os.getenv("PS_USER")
     user_config["Telegram"]["telegram_bot_token"] = os.getenv("TELEGRAM_TOKEN")
     user_config["Telegram"]["authorized_users"] = [int(os.getenv("TG_USER"))]
     user_config["Bot"]["private_ip"] = os.getenv("PRIVIP")
     user_config["Bot"]["domain"] = os.getenv("DOMAIN")
-    user_config["Pocketsmith"]["user"] = os.getenv("PS_USER")
 
     WEBHOOK_URL_BASE = "https://%s:%s" % (
         user_config["Bot"]["domain"],
